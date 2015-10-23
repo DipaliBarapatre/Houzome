153e929fd809819a1f5f03903bd3049d4f2790fe
affimintus <dipalibarapatre@gmail.com>
2015-10-23 23:28:02 +0530

    Assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.serve_static_assets = true
 
   config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 
